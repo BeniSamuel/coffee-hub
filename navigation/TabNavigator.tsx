119700f6a734b6f137393583dd947fea08b7d66c
benisamuel <benisamuel566@gmail.com>
2025-07-15 16:41:24 +0200

    added tab bar icons

--- navigation/TabNavigator.tsx
+++ navigation/TabNavigator.tsx
@@ -1,4 +1,5 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Image } from "react-native";
 import Home from "../screens/tabs/Home/Home";
 import Cart from "../screens/tabs/Cart/Cart";
 import Favorite from "../screens/tabs/Favorite/Favorite";
@@ -10,14 +11,52 @@ const TabNavigator = () => {
   return (
     <Tab.Navigator
       initialRouteName="Home"
-      screenOptions={{
+      screenOptions={({ route }) => ({
         headerShown: false,
+        tabBarShowLabel: false,
         tabBarStyle: {
           backgroundColor: "#0C0F14",
-          outlineColor: "none",
-          borderColor: "none",
+          borderTopWidth: 0,
+          elevation: 0,
         },
-      }}
+
+        tabBarIcon: ({ focused, size }) => {
+          let icon;
+
+          switch (route.name) {
+            case "Home":
+              icon = focused
+                ? require("../assets/common/home-active-icon.png")
+                : require("../assets/common/home-inactive-icon.png");
+              break;
+            case "Cart":
+              icon = focused
+                ? require("../assets/common/cart-active-icon.png")
+                : require("../assets/common/cart-inactive-icon.png");
+              break;
+            case "Favorite":
+              icon = focused
+                ? require("../assets/common/favorite-active-icon.png")
+                : require("../assets/common/favorite-inactive-icon.png");
+              break;
+            case "History":
+              icon = focused
+                ? require("../assets/common/order-active-icon.png")
+                : require("../assets/common/order-inactive-icon.png");
+              break;
+            default:
+              icon = require("../assets/common/default-icon.png");
+          }
+
+          return (
+            <Image
+              source={icon}
+              style={{ width: size, height: size }}
+              resizeMode="contain"
+            />
+          );
+        },
+      })}
     >
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Cart" component={Cart} />
