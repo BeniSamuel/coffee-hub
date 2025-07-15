9b1e38ea440759b013250448185766a150473e89
benisamuel <benisamuel566@gmail.com>
2025-07-15 21:17:33 +0200

    added coffee data

---
+++ data/coffee-milk/coffee.data.ts
@@ -0,0 +1,66 @@
+import coffee from "../../types/coffee.type";
+
+const coffee_milk: coffee[] = [
+  {
+    id: 1,
+    image: require("../../assets/home/coffee-milk/coffee-milk-one.png"),
+    name: "Cappuccino",
+    origin: "From Africa",
+    made_of: "With Streamed Milk",
+    rating: 4.2,
+    description:
+      "Arabica beans are by far the most popular type of coffee beans, making up about 60% of the world’s coffee. These tasty beans originated many centuries ago in the highlands of Ethiopia, and may even be the first coffee beans ever consumed! ",
+    size: 250,
+    price: 10.5,
+  },
+  {
+    id: 2,
+    image: require("../../assets/home/coffee-milk/coffee-milk-two.png"),
+    name: "Cappuccino",
+    origin: "From America",
+    made_of: "With Foam",
+    rating: 4.5,
+    description:
+      "Arabica beans are by far the most popular type of coffee beans, making up about 60% of the world’s coffee. These tasty beans originated many centuries ago in the highlands of Ethiopia, and may even be the first coffee beans ever consumed! ",
+    size: 500,
+    price: 4.20,
+  },
+  {
+    id: 3,
+    image: require("../../assets/home/coffee-milk/coffee-milk-one.png"),
+    name: "Cappuccino",
+    origin: "From Africa",
+    made_of: "With Streamed Milk",
+    rating: 4.2,
+    description:
+      "Arabica beans are by far the most popular type of coffee beans, making up about 60% of the world’s coffee. These tasty beans originated many centuries ago in the highlands of Ethiopia, and may even be the first coffee beans ever consumed! ",
+    size: 250,
+    price: 10.5,
+  },
+  {
+    id: 4,
+    image: require("../../assets/home/coffee-milk/coffee-milk-two.png"),
+    name: "Cappuccino",
+    origin: "From Africa",
+    made_of: "With Streamed Milk",
+    rating: 4.2,
+    description:
+      "Arabica beans are by far the most popular type of coffee beans, making up about 60% of the world’s coffee. These tasty beans originated many centuries ago in the highlands of Ethiopia, and may even be the first coffee beans ever consumed! ",
+    size: 250,
+    price: 10.5,
+  },
+  {
+    id: 5,
+    image: require("../../assets/home/coffee-milk/coffee-milk-one.png"),
+    name: "Cappuccino",
+    origin: "From Africa",
+    made_of: "With Streamed Milk",
+    rating: 4.2,
+    description:
+      "Arabica beans are by far the most popular type of coffee beans, making up about 60% of the world’s coffee. These tasty beans originated many centuries ago in the highlands of Ethiopia, and may even be the first coffee beans ever consumed! ",
+    size: 250,
+    price: 10.5,
+  },
+];
+
+export default coffee_milk;
