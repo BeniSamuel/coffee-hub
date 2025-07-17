a72ee18b5b49fafc3291de8c31eea86ee3f73a03
benisamuel <benisamuel566@gmail.com>
2025-07-17 14:13:45 +0200

    added coffee-milk data

--- data/coffee-milk/coffee.data.ts
+++ data/coffee-milk/coffee.data.ts
@@ -21,7 +21,7 @@ const coffee_milk: coffee[] = [
     made_of: "With Foam",
     rating: 4.5,
     description:
-      "Arabica beans are by far the most popular type of coffee beans, making up about 60% of the world’s coffee. These tasty beans originated many centuries ago in the highlands of Ethiopia, and may even be the first coffee beans ever consumed! ",
+      "Cappuccino is a latte made with more foam than steamed milk, often with a sprinkle of cocoa powder or cinnamon on top.",
     size: 500,
     price: 4.20,
   },
@@ -34,7 +34,7 @@ const coffee_milk: coffee[] = [
     rating: 4.2,
     description:
       "Arabica beans are by far the most popular type of coffee beans, making up about 60% of the world’s coffee. These tasty beans originated many centuries ago in the highlands of Ethiopia, and may even be the first coffee beans ever consumed! ",
-    size: 250,
+    size: 1000,
     price: 10.5,
   },
   {
@@ -45,7 +45,7 @@ const coffee_milk: coffee[] = [
     made_of: "With Streamed Milk",
     rating: 4.2,
     description:
-      "Arabica beans are by far the most popular type of coffee beans, making up about 60% of the world’s coffee. These tasty beans originated many centuries ago in the highlands of Ethiopia, and may even be the first coffee beans ever consumed! ",
+      "Cappuccino is a latte made with more foam than steamed milk, often with a sprinkle of cocoa powder or cinnamon on top.",
     size: 250,
     price: 10.5,
   },
@@ -58,7 +58,7 @@ const coffee_milk: coffee[] = [
     rating: 4.2,
     description:
       "Arabica beans are by far the most popular type of coffee beans, making up about 60% of the world’s coffee. These tasty beans originated many centuries ago in the highlands of Ethiopia, and may even be the first coffee beans ever consumed! ",
-    size: 250,
+    size: 500,
     price: 10.5,
   },
 ];
