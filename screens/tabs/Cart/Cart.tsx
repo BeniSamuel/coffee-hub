ab887d308c0cfab37bc345064359ef63ff22ecc5
benisamuel <benisamuel566@gmail.com>
2025-07-17 16:23:31 +0200

    added cart data

--- screens/tabs/Cart/Cart.tsx
+++ screens/tabs/Cart/Cart.tsx
@@ -1,19 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import TabLayout from '../../../layout/TabLayout'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import TabLayout from "../../../layout/TabLayout";
+import CartsData from "../../../components/cart/CartsData";
 
 const Cart = () => {
   return (
     <TabLayout screen_name="Cart">
-        <View>
-            <Text style={{color: "white", fontFamily: "poppins-regular"}}>
-                Cart
-            </Text>
-        </View>
+      <CartsData />
     </TabLayout>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
