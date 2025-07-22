4e4387b567140aaab304fbebd53c303606f017c0
benisamuel <benisamuel566@gmail.com>
2025-07-22 20:20:32 +0200

    added favorite tab layout

--- screens/tabs/Favorite/Favorite.tsx
+++ screens/tabs/Favorite/Favorite.tsx
@@ -1,13 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import TabLayout from "../../../layout/TabLayout";
+import FavoriteData from "../../../components/favorite/FavoriteData";
 
 const Favorite = () => {
   return (
     <TabLayout screen_name="Favorites">
-      <View>
-        <Text>Favorite</Text>
-      </View>
+      <FavoriteData />
     </TabLayout>
   );
 };
