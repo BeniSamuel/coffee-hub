2a5801510c00c74a06306c5699ecb49640c12899
benisamuel <benisamuel566@gmail.com>
2025-07-22 20:20:54 +0200

    added history used tab

--- screens/tabs/History/History.tsx
+++ screens/tabs/History/History.tsx
@@ -1,13 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import TabLayout from "../../../layout/TabLayout";
+import HistoryData from "../../../components/history/HistoryData";
 
 const History = () => {
   return (
     <TabLayout screen_name="Order History">
-      <View>
-        <Text>History</Text>
-      </View>
+      <HistoryData />
     </TabLayout>
   );
 };
